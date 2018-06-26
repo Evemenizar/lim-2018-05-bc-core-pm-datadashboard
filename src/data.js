2731ab1f9d8d83957a7bb3b3ffcca3671135c3af
evelyn <you@example.com>
2018-06-26 16:12:12 -0500

    subiendo cambios en nueva rama

--- src/data.js
+++ src/data.js
@@ -1,19 +1,14 @@
+const header = document.getElementById("header");
+const laboratoriapage = document.getElementById("start");
+const buttonDirectory = document.getElementById("directory");
+const studentPage = document.getElementById("studentPage");
+const select = document.getElementById("cohort");
 
-fetch('../data/cohorts.json')
-.then(response => response.json())
-.then(json => {
-  const cohort = json;
-  console.log(cohort.length);
-  
-  for (let i = 0; i < cohort.length; i++) {
-    const options = document.createElement('option');
-    const contentoption = document.createTextNode(cohort[i].id);
-   options.appendChild(contentoption);
-   console.log(cohort[i].id);
-     select.appendChild(options);
-    
-  }
-})
-.catch((err) => {
-console.error(err);
-});
+buttonDirectory.addEventListener('click', ()=>{
+    studentPage.style.display="block";
+    laboratoriapage.style.display="none";
+})  
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-const buttonWelcome = document.getElementById("buttonhome");
-const laboratoriapage=document.getElementById("start");
-const welcomePage=document.getElementById("welcomepage");
-const select = document.getElementById('cohort');
+window.computerUsersStats = (user, progress, courses) =>{
 
-buttonWelcome.addEventListener('click', ()=>{
-    laboratoriapage.style.display="block";
-    welcomePage.style.display="none";
-})
+  fetch("..\Data-dashboard\lim-2018-05-bc-core-pm-datadashboard\data\cohorts\lim-2018-03-pre-core-pw")
+  .then(response =>{
+    if(response.status == 200){ 
+    }else{
+       
+    };}, 
+    response.json())
+  .then(response =>{
+    console.log(response)
+  })
+}
 
+/*
+  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  .then(response => response.json())
+  .then(response => {
+  const cohort = json;
+ 
+  for (let i = 0; i < cohort.length; i++) {
+    const options = document.createElement('option');
+    const contentoption = document.createTextNode(cohort[i].name);
+    options.appendChild(contentoption);
 
+     select.appendChild(options);   
+  }
 
-  
+})   
+.catch((err) => {
+
+});
+
+*/
