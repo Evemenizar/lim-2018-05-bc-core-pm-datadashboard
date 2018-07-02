ee385ea527f2e6f7a298d9dab126444c65b61179
evelyn <you@example.com>
2018-07-02 15:25:38 -0500

    cambios en html

--- src/data.js
+++ src/data.js
@@ -2,17 +2,27 @@ const header = document.getElementById("header");
 const laboratoriapage = document.getElementById("start");
 const buttonDirectory = document.getElementById("directory");
 const studentPage = document.getElementById("studentPage");
-const select = document.getElementById("cohort");
-const filSede = document.getElementById("filSede");
-const filGeneration = document.getElementById("filGeneration");
-const filAlphabet = document.getElementById("filAlphabet");
+const cohortsPage = document.getElementById("cohortsPage");
+const sedesCohort = document.getElementById("sedesCohort");
+const studentcohort = document.getElementById("studentcohort");
+const cohort1 = document.getElementById("cohort");
 
 buttonDirectory.addEventListener('click', ()=>{
     studentPage.style.display="block";
-    filSede.style.display="block";
-    filGeneration.style.display="block";
-    filAlphabet.style.display="block";
+    cohortsPage.style.display="block";
     laboratoriapage.style.display="none";
 })  
+
+const link1 = '../data/cohorts.json'
+const link2 =  '../data/cohorts/lim-2018-03-pre-core-pw/users.json'
+const link3 = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'
+
+const callsLink = []
+let user = []
+ 
+let progress = []
+let cohort = []
+
+
 
 
--- src/main.js
+++ src/main.js
@@ -1,35 +1,28 @@
-
-window.computerUsersStats = (user, progress, courses) =>{
-
-  fetch("..\Data-dashboard\lim-2018-05-bc-core-pm-datadashboard\data\cohorts\lim-2018-03-pre-core-pw")
-  .then(response =>{
-    if(response.status == 200){ 
-    }else{
-       
-    };}, 
-    response.json())
-  .then(response =>{
-    console.log(response)
-  })
-}
-
-/*
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
-  .then(response => response.json())
-  .then(response => {
-  const cohort = json;
- 
-  for (let i = 0; i < cohort.length; i++) {
-    const options = document.createElement('option');
-    const contentoption = document.createTextNode(cohort[i].name);
-    options.appendChild(contentoption);
-
-     select.appendChild(options);   
+fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+.then(function(response) {
+  if(response.ok) {
+    response.json()
+.then(function(json) {
+      user = json;
+      cohorts();
+    });
+  } else {
+    console.log('error');
   }
-
-})   
-.catch((err) => {
-
 });
+const cohorts = () => {
+  for (let i = 0; i < user.length; i++) {
+    const options = document.createElement('option');
+    const contentoption = document.createTextNode(user[i].name);
+   options.appendChild(contentoption);
+   console.log(user[i].id);
+     cohort1.appendChild(options);
+     }
+const resul = resuls [i];
+     if(resul.id=== "00hJv4mzvqM3D9kBy3dfxoJyFV82") {
+       return resul;
+      }
 
-*/
+
+
+}
